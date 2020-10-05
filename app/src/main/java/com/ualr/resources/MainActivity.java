22c1d9fd10555e842dbb5fc8dd98a835135dc527
irconde <irconde@ualr.edu>
2020-10-05 15:13:10 -0500

    First commit

---
+++ app/src/main/java/com/ualr/resources/MainActivity.java
@@ -0,0 +1,23 @@
+package com.ualr.resources;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.snackbar.Snackbar;
+
+// TODO 3: Define the alternative resources needed to have different content and look n feel depending on the device language.
+// TODO 4: Get the description string value from resources
+// TODO 5: Initialize the text property of the TextView element with the "country_description_text" id by using the value retrieved in the previous TODO point
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
